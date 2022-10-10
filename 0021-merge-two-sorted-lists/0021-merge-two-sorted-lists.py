4e1883c28ce51fd661e4b8c9b063affc293121ab
Geonsik Moon <gsmoon97@gmail.com>
2022-10-10 21:58:57 +0800

    Time: 71 ms (41.69%), Space: 13.9 MB (32.59%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -7,9 +7,10 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         ptr1 = list1
         ptr2 = list2
-        prev = ListNode()
-        dummy = prev
+        prev = ListNode() # pointer for the merged linked list
+        dummy = prev # dummy pointer to return the head of the merged linked list
         
+        # traverse until one of the list reaches the end
         while ptr1 and ptr2:
             if ptr1.val < ptr2.val:
                 prev.next = ptr1
@@ -19,6 +20,7 @@ class Solution:
                 prev.next = ptr2
                 ptr2 = ptr2.next
                 prev = prev.next
+        # merge the remaining linked list
         if ptr1:
             prev.next = ptr1
         else:
